b804fa319202808e780e91180cd6b2e975dda0c6
TheRedKardinal <claudio_giasi@icloud.com>
2026-06-12 16:36:18 +0200

    Modale finita

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,17 @@
 // Aggiungi qui il codice JavaScript se serve.
 // Per la Versione Base spesso non serve niente: tutto si fa in HTML+SCSS.
 // Per le versioni Intermedia e Avanzata: filtri lavori, toggle tema, validazione form.
+
+// Dico a JS di intercettare e di trattare l'elemento come un modale
+const modal = new bootstrap.Modal(document.getElementById('cardModal'));
+const card = document.querySelectorAll('#servizi .card');
+const modalTitle = document.getElementById('cardModalTitle');
+const modalBody = document.getElementById('cardModalBody')
+
+card.forEach(c => {
+    c.addEventListener('click', (e) => {
+    modalTitle.textContent = c.dataset.title;
+    modalBody.textContent = c.dataset.text;
+    modal.show();
+    })
+});
