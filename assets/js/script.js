1e17f546710651de0187fccf76ffd6d86a7d7bc9
TheRedKardinal <claudio_giasi@icloud.com>
2026-06-12 17:44:39 +0200

    Ultimato la compilazione del form con stampa dei valori subito sotto al submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,13 +13,15 @@ const modalTitle = document.getElementById('cardModalTitle');
 const modalBody = document.getElementById('cardModalBody');
 const body = document.querySelector('body');
 const darkModeToggle = document.getElementById('dark-mode');
-const submitForm = document.querySelector('form');
+const form = document.querySelector('form');
+const formOutput = document.getElementById('form-output');
+
 
 card.forEach(c => {
     c.addEventListener('click', (e) => {
-    modalTitle.textContent = c.dataset.title;
-    modalBody.textContent = c.dataset.text;
-    modal.show();
+        modalTitle.textContent = c.dataset.title;
+        modalBody.textContent = c.dataset.text;
+        modal.show();
     })
 });
 
@@ -28,6 +30,30 @@ darkModeToggle.addEventListener('click', () => {
 
 });
 
-submitForm.addEventListener('submit', (e) => {
+form.addEventListener('submit', (e) => {
     e.preventDefault();
-})
+
+    const nameValue = document.getElementById('name').value;
+    const nameOutput = document.createElement('p');
+    nameOutput.textContent = 'Nome: ' + nameValue;
+
+    const emailValue = document.getElementById('email').value;
+    const emailOutput = document.createElement('p');
+    emailOutput.textContent = 'Email: ' + emailValue;
+
+    const projectValue = document.querySelector('#project-type option:checked').textContent;
+    const projectOutput = document.createElement('p');
+    projectOutput.textContent = 'Progetto: ' + projectValue;
+
+    const messageValue = document.getElementById('message').value;
+    const messageOutput = document.createElement('p');
+    messageOutput.textContent = 'Messaggio: ' + messageValue;
+
+    formOutput.classList.remove('d-none');
+    formOutput.classList.add('mt-4', 'p-3');
+    
+    formOutput.appendChild(nameOutput);
+    formOutput.appendChild(emailOutput);
+    formOutput.appendChild(projectOutput);
+    formOutput.appendChild(messageOutput);
+});
